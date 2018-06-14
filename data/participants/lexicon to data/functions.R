3db4f592d12b84443b1e27a41d1b13d615890445
Victor Rouco <victorrouco@ub.edu>
2018-06-14 02:26:58 +0200

    created functions file

---
+++ data/participants/lexicon to data/functions.R
@@ -0,0 +1,13 @@
+
+
+summary.data <- function(data){
+  summary <- data %>% #data has to be as data$lexicon es.csv
+  dplyr::count(sentiment, created,index = created, wt = log1p(retweet))%>%
+  ungroup() %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(n = (joy + trust + anger + fear + sadness + disgust + surprise),
+         joy=(joy/n), trust=(trust/n), anger=(anger/n), fear=fear/n,surprise=surprise/n,
+         
+         sadness=sadness/n, disgust=disgust/n)%>%
+  ungroup()
+  return(summary)}
